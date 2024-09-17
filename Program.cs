ec0d3a15b53a778b7f21be413eefadeca301b16f
Josue Bautista <josubauti@outlook.com>
2024-09-17 13:15:35 -0600

    finalice
    Se finalizo todo lo solicitado

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ static void create_file_json(string titulo, string datos){
     }
     else
     {
-        Console.WriteLine("La carpeta ya existe.");
+        Console.WriteLine(" ");
     }
 
     for (int j = 0; j < (docs + 1); j++) {
@@ -183,7 +183,7 @@ static void abrir(){
     // Nombre del archivo que estás buscando
     string fileNameToFind = "tablaFAT.json";
 
-    Console.WriteLine("Que opcion deseas realizar");
+    Console.WriteLine("¿Que archivo deseas abrir?");
     string op = Console.ReadLine()!;
     int opcionSolicitada = int.Parse(op);
     int k = 1;
@@ -257,6 +257,202 @@ static void abrir(){
 }
 
 
+static void modificar(){
+    listar();
+
+    // Directorio principal donde quieres buscar
+    string rootDirectory = @"C:/Users/artha/Desktop/Cuarto Semestre/Manejo e implementacion/Repo ProyectoFAT/proyectoTablaFAT";
+
+    // Nombre del archivo que estás buscando
+    string fileNameToFind = "tablaFAT.json";
+
+    Console.WriteLine("¿Que archivo deseas modificar?");
+    string op = Console.ReadLine()!;
+    int opcionSolicitada = int.Parse(op);
+    int k = 1;
+    if (Directory.Exists(rootDirectory))
+    {
+        foreach (string subdirectory in Directory.GetDirectories(rootDirectory, "*", SearchOption.AllDirectories))
+        {
+            string filePath = Path.Combine(subdirectory, fileNameToFind);
+
+            // Verifica si el archivo existe en la subcarpeta actual
+            if (File.Exists(filePath))
+            {
+                string actual_directory = Directory.GetCurrentDirectory();
+                string jsonFromFile = File.ReadAllText(filePath);
+                List<TablaFAT> deserializedFAT = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile)!;
+
+                foreach(TablaFAT tablaFAT in deserializedFAT){
+                    if (tablaFAT.papelera is false){
+                        if (tablaFAT.fechaModificacion == ""){
+                            if (opcionSolicitada == k){
+                                Console.WriteLine($"{k}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}\n   Fecha de creación: {tablaFAT.fechaCreacion}");
+                                
+                                string actual_directory_new = Directory.GetCurrentDirectory();
+                                string filePath_new = $"{actual_directory}/{tablaFAT.nombreArchivo}";
+                                int numeroDeDocumentos = Directory.GetFiles(filePath_new).Length;
+                                for (int l = 1; l < numeroDeDocumentos + 1; l++){
+                                    string filePath_new_write = $"{actual_directory}/{tablaFAT.nombreArchivo}/{tablaFAT.nombreArchivo}{l}.json";
+                                    File.Delete(filePath_new_write);
+                                }
+                                Console.WriteLine("Ingrese los nuevos datos del archivo: ");
+                                if (Console.ReadKey().Key != ConsoleKey.Escape){
+                                    string datos = Console.ReadLine()!;
+                                    if (Console.ReadKey().Key == ConsoleKey.Escape){
+                                        create_file_json(tablaFAT.nombreArchivo, datos);
+                                    }
+                                }
+                                
+                                tablaFAT.fechaModificacion = DateTime.Now.ToString();
+
+                                string actual_directory_newFAT = Directory.GetCurrentDirectory();
+                                string filePath_newFAT = $"{actual_directory}/{tablaFAT.nombreArchivo}/tablaFAT.json";
+                                
+                                TablaFAT tablaFAT_new = new TablaFAT(tablaFAT.nombreArchivo, tablaFAT.rutaDatosInicial, tablaFAT.papelera, tablaFAT.cantidadDatos, tablaFAT.fechaCreacion, tablaFAT.fechaModificacion, tablaFAT.fechaEliminacion);
+                                try{
+                                    string jsonFromFile_new = File.ReadAllText(filePath);
+                                    List<TablaFAT> deserializedText = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile_new)!;
+                                    deserializedText.Clear();
+                                    deserializedText.Add(tablaFAT_new); 
+                                    string jsonString = JsonSerializer.Serialize(deserializedText);
+                                    File.WriteAllText(filePath, jsonString);
+                                }
+                                catch (Exception)
+                                {
+                                    List<TablaFAT> info = new List<TablaFAT> { tablaFAT };
+                                    string jsonString2 = JsonSerializer.Serialize(info);
+                                    File.WriteAllText(filePath, jsonString2);
+                                }
+                            }
+                        } else {
+                            if (opcionSolicitada == k){
+                                Console.WriteLine($"{k}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}\n   Fecha de creación: {tablaFAT.fechaCreacion}\n   Fecha de modificación: {tablaFAT.fechaModificacion}");
+
+                                string actual_directory_new = Directory.GetCurrentDirectory();
+                                string filePath_new = $"{actual_directory}/{tablaFAT.nombreArchivo}";
+                                int numeroDeDocumentos = Directory.GetFiles(filePath_new).Length;
+                                for (int l = 1; l < numeroDeDocumentos + 1; l++){
+                                    string filePath_new_write = $"{actual_directory}/{tablaFAT.nombreArchivo}/{tablaFAT.nombreArchivo}{l}.json";
+                                    File.Delete(filePath_new_write);
+                                }
+                                Console.WriteLine("Ingrese los nuevos datos del archivo: ");
+                                if (Console.ReadKey().Key != ConsoleKey.Escape){
+                                    string datos = Console.ReadLine()!;
+                                    if (Console.ReadKey().Key == ConsoleKey.Escape){
+                                        create_file_json(tablaFAT.nombreArchivo, datos);
+                                    }
+                                }
+                                
+                                tablaFAT.fechaModificacion = DateTime.Now.ToString();
+
+                                string actual_directory_newFAT = Directory.GetCurrentDirectory();
+                                string filePath_newFAT = $"{actual_directory}/{tablaFAT.nombreArchivo}/tablaFAT.json";
+                                
+                                TablaFAT tablaFAT_new = new TablaFAT(tablaFAT.nombreArchivo, tablaFAT.rutaDatosInicial, tablaFAT.papelera, tablaFAT.cantidadDatos, tablaFAT.fechaCreacion, tablaFAT.fechaModificacion, tablaFAT.fechaEliminacion);
+                                try{
+                                    string jsonFromFile_new = File.ReadAllText(filePath);
+                                    List<TablaFAT> deserializedText = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile_new)!;
+                                    deserializedText.Clear();
+                                    deserializedText.Add(tablaFAT_new); 
+                                    string jsonString = JsonSerializer.Serialize(deserializedText);
+                                    File.WriteAllText(filePath, jsonString);
+                                }
+                                catch (Exception)
+                                {
+                                    List<TablaFAT> info = new List<TablaFAT> { tablaFAT };
+                                    string jsonString2 = JsonSerializer.Serialize(info);
+                                    File.WriteAllText(filePath, jsonString2);
+                                }
+                            }
+                        }
+                        k ++;
+                    }
+                }
+            }
+        }
+    }
+    else{
+        Console.WriteLine("El directorio especificado no existe.");
+    }
+}
+
+
+static void eliminar(){
+
+    listar();
+
+    // Directorio principal donde quieres buscar
+    string rootDirectory = @"C:/Users/artha/Desktop/Cuarto Semestre/Manejo e implementacion/Repo ProyectoFAT/proyectoTablaFAT";
+
+    // Nombre del archivo que estás buscando
+    string fileNameToFind = "tablaFAT.json";
+
+    Console.WriteLine("¿Que opcion deseas eliminar?");
+    string op = Console.ReadLine()!;
+    int opcionSolicitada = int.Parse(op);
+    int k = 1;
+    if (Directory.Exists(rootDirectory))
+    {
+        foreach (string subdirectory in Directory.GetDirectories(rootDirectory, "*", SearchOption.AllDirectories))
+        {
+            string filePath = Path.Combine(subdirectory, fileNameToFind);
+
+            // Verifica si el archivo existe en la subcarpeta actual
+            if (File.Exists(filePath))
+            {
+                string actual_directory = Directory.GetCurrentDirectory();
+                string jsonFromFile = File.ReadAllText(filePath);
+                List<TablaFAT> deserializedFAT = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile)!;
+
+                foreach(TablaFAT tablaFAT in deserializedFAT){
+                    if (tablaFAT.papelera is false){
+                        if (opcionSolicitada == k){
+                            Console.WriteLine($"{k}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}");
+                            Console.WriteLine("¿Seguro que quieres eliminarlo? (si/no)");
+                            string op_delete = Console.ReadLine()!;
+                            if (op_delete == "si"){
+
+                                tablaFAT.papelera = true;
+                                tablaFAT.fechaEliminacion = DateTime.Now.ToString();
+
+                                string actual_directory_new = Directory.GetCurrentDirectory();
+                                string filePath_new = $"{actual_directory}/{tablaFAT.nombreArchivo}/tablaFAT.json";
+                                
+                                TablaFAT tablaFAT_new = new TablaFAT(tablaFAT.nombreArchivo, tablaFAT.rutaDatosInicial, tablaFAT.papelera, tablaFAT.cantidadDatos, tablaFAT.fechaCreacion, tablaFAT.fechaModificacion, tablaFAT.fechaEliminacion);
+                                try{
+                                    string jsonFromFile_new = File.ReadAllText(filePath);
+                                    List<TablaFAT> deserializedText = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile_new)!;
+                                    deserializedText.Clear();
+                                    deserializedText.Add(tablaFAT_new); 
+                                    string jsonString = JsonSerializer.Serialize(deserializedText);
+                                    File.WriteAllText(filePath, jsonString);
+                                }
+                                catch (Exception)
+                                {
+                                    List<TablaFAT> info = new List<TablaFAT> { tablaFAT };
+                                    string jsonString2 = JsonSerializer.Serialize(info);
+                                    File.WriteAllText(filePath, jsonString2);
+                                }
+                            }
+                            else if (op_delete == "no"){
+                                return;
+                                }
+                            Console.WriteLine("El archivo ha sido eliminado correctamente");
+                            
+                        }
+                        k ++;
+                    }
+                }
+            }
+        }
+    }
+    else{
+        Console.WriteLine("El directorio especificado no existe.");
+    }
+}
+
+
 static void recuperar(){
 
     // Directorio principal donde quieres buscar
@@ -284,11 +480,7 @@ static void recuperar(){
 
                 foreach(TablaFAT tablaFAT in deserializedFAT){
                     if (tablaFAT.papelera is true){
-                        if (tablaFAT.fechaModificacion == ""){
-                            Console.WriteLine($"{i}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}\n   Fecha de creación: {tablaFAT.fechaCreacion}");
-                        } else {
-                            Console.WriteLine($"{i}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}\n   Fecha de creación: {tablaFAT.fechaCreacion}\n   Fecha de modificación: {tablaFAT.fechaModificacion}");
-                        }
+                        Console.WriteLine($"{i}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}\n   Fecha de eliminación: {tablaFAT.fechaEliminacion}");
                         i ++;
                     }
                 }
@@ -297,6 +489,68 @@ static void recuperar(){
     }
     else
     {
+        Console.WriteLine("El directorio especificado no existe.");
+    }
+
+    Console.WriteLine("¿Que opcion deseas recuperar?");
+    string op = Console.ReadLine()!;
+    int opcionSolicitada = int.Parse(op);
+    int k = 1;
+    if (Directory.Exists(rootDirectory))
+    {
+        foreach (string subdirectory in Directory.GetDirectories(rootDirectory, "*", SearchOption.AllDirectories))
+        {
+            string filePath = Path.Combine(subdirectory, fileNameToFind);
+
+            // Verifica si el archivo existe en la subcarpeta actual
+            if (File.Exists(filePath))
+            {
+                string actual_directory = Directory.GetCurrentDirectory();
+                string jsonFromFile = File.ReadAllText(filePath);
+                List<TablaFAT> deserializedFAT = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile)!;
+
+                foreach(TablaFAT tablaFAT in deserializedFAT){
+                    if (tablaFAT.papelera is true){
+                        if (opcionSolicitada == k){
+                            Console.WriteLine($"{k}) Nombre del archivo: {tablaFAT.nombreArchivo}\n   Tamaño de caracteres: {tablaFAT.cantidadDatos}");
+                            Console.WriteLine("¿Seguro que quieres recuperarlo? (si/no)");
+                            string op_delete = Console.ReadLine()!;
+                            if (op_delete == "si"){
+
+                                tablaFAT.papelera = false;
+                                tablaFAT.fechaEliminacion = "";
+
+                                string actual_directory_new = Directory.GetCurrentDirectory();
+                                string filePath_new = $"{actual_directory}/{tablaFAT.nombreArchivo}/tablaFAT.json";
+                                
+                                TablaFAT tablaFAT_new = new TablaFAT(tablaFAT.nombreArchivo, tablaFAT.rutaDatosInicial, tablaFAT.papelera, tablaFAT.cantidadDatos, tablaFAT.fechaCreacion, tablaFAT.fechaModificacion, tablaFAT.fechaEliminacion);
+                                try{
+                                    string jsonFromFile_new = File.ReadAllText(filePath);
+                                    List<TablaFAT> deserializedText = JsonSerializer.Deserialize<List<TablaFAT>>(jsonFromFile_new)!;
+                                    deserializedText.Clear();
+                                    deserializedText.Add(tablaFAT_new); 
+                                    string jsonString = JsonSerializer.Serialize(deserializedText);
+                                    File.WriteAllText(filePath, jsonString);
+                                }
+                                catch (Exception)
+                                {
+                                    List<TablaFAT> info = new List<TablaFAT> { tablaFAT };
+                                    string jsonString2 = JsonSerializer.Serialize(info);
+                                    File.WriteAllText(filePath, jsonString2);
+                                }
+                            }
+                            else if (op_delete == "no"){
+                                return;
+                                }
+                            Console.WriteLine("El archivo ha sido recuperado correctamente");
+                        }
+                        k ++;
+                    }
+                }
+            }
+        }
+    }
+    else{
         Console.WriteLine("El directorio especificado no existe.");
     }
 }
@@ -335,14 +589,13 @@ static void main() {
 
         if (op == "4") {
             Console.WriteLine("MODIFICAR ARCHIVO");
+            modificar();
 
         }
 
         if (op == "5") {
             Console.WriteLine("ELIMINAR ARCHIVO");
-
-            Console.WriteLine("Ingresa la palabra a buscar:");
-            string palabra_buscar = Console.ReadLine()!;
+            eliminar();
 
         }
 
